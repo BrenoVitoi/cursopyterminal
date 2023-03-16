b30892a0a5a7fd659644e0dbc52aa1ef51c0fce1
Breno Vitoi <breno.vitoi@gmail.com>
2023-03-16 14:14:35 -0300

    Criando conexao do banco

--- tutorial.py
+++ tutorial.py
@@ -32,6 +32,7 @@ class Manager:
         time.sleep(0.05)
         print()
         print("1 :) add")
+        time.sleep(0.05)
 
         print("2 :) Remove")
         time.sleep(0.05)
@@ -55,7 +56,7 @@ class Manager:
             self.update()
 
         elif opcao == "4":
-            self.list()
+            self.get_list()
 
         elif opcao == "5":
             self.terminate()
@@ -64,9 +65,34 @@ class Manager:
             winsound.Beep(2500, 100)
             print("ERROR, TENTA NOVAMENTE AS OPCOES 1-5")
             time.sleep(2)
+            self.menu()
 
     def main(self):
         os.system("cls")
+        if os.path.isfile("connection"):
+            db = sqlite3.connect("connection")
+            time.sleep(3)
+            winsound.Beep(2000, 50)
+            print("CONECTADO AO BANCO DE DADOS")
+            time.sleep(3)
+            self.menu()
+        else:
+            print("ESTA CONEXAO  NAO EXISTE")
+            print()
+            time.sleep(3)
+            WindowsError(2000, 50)
+
+            print("CRIANDO NOVA CONEXAO COM O BANCO DE DADO")
+            time.sleep(3)
+            db = sqlite3.connect("connection")
+            winsound.Beep(2000, 50)
+            print()
+
+            print("Conexao criada com sucesso")
+            print("Conectado com sucesso")
+            time.sleep(3)
+            self.menu()
+
         self.menu()
 
 
