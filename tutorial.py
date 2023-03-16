5ea9c9a2b0a8841e5f92924ae6cb469eb38dedc4
Breno Vitoi <breno.vitoi@gmail.com>
2023-03-16 11:40:51 -0300

    first commit

---
+++ tutorial.py
@@ -0,0 +1,29 @@
+import sqlite3
+import os
+import time
+import winsound
+
+
+class Manager:
+    def __init__(self):
+        self.name = ""
+        self.phone = ""
+        self.address = ""
+
+    def add(self):
+        pass
+
+    def update(self):
+        pass
+
+    def remove(self):
+        pass
+
+    def get_list(self):
+        pass
+
+    def terminate(self):
+        pass
+
+    def add(self):
+        pass
