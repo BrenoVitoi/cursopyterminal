fd3c80e5965eea8244c82c5dd55d808abc32eb7d
Breno Vitoi <breno.vitoi@gmail.com>
2023-03-16 13:41:45 -0300

    Implementando a Lista

--- tutorial.py
+++ tutorial.py
@@ -27,18 +27,46 @@ class Manager:
 
     def menu(self):
         os.system("cls")
+        winsound.Beep(2000, 50)
         print("-------------Menu----------------")
+        time.sleep(0.05)
+        print()
         print("1 :) add")
 
         print("2 :) Remove")
+        time.sleep(0.05)
 
         print("3 :) Update")
+        time.sleep(0.05)
 
         print("4 :) List")
+        time.sleep(0.05)
 
         print("5 :) Terminate")
+        print()
+
+        opcao = input("SELECIONE UMA ACAO :")
+        if opcao == "1":
+            self.add()
+        elif opcao == "2":
+            self.remove()
+
+        elif opcao == "3":
+            self.update()
+
+        elif opcao == "4":
+            self.list()
+
+        elif opcao == "5":
+            self.terminate()
+
+        else:
+            winsound.Beep(2500, 100)
+            print("ERROR, TENTA NOVAMENTE AS OPCOES 1-5")
+            time.sleep(2)
 
     def main(self):
+        os.system("cls")
         self.menu()
 
 
